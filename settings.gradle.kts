b6d44cd61956f909c07d00a021a3cabc4da27b29
yasmine muhamed <74177304+yasmine-yas@users.noreply.github.com>
2023-10-28 19:07:43 +0200

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Custom Fan Controller"
+include(":app")
+ 
